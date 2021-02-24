400c9ebb2e5c82a7b228305156d8190854630562
Liydmyla Chubko <59334344+Chubko@users.noreply.github.com>
2021-02-24 17:59:33 +0200

    HW3 Routes, controllers, middlewars, services

--- homework/homework3/router/user.router.js
+++ homework/homework3/router/user.router.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 const userController = require('../controller/user.controller');
 const userMiddleware = require('../middleware/user.middleware');
 
-router.get('/', userController.getAllUsers);
+router.get('/',userController.getAllUsers);
+
+router.get('/',  userController.getUserByName);
 
 router.post('/', userMiddleware.isUserValid, userController.createUser);
 
-router.get('/:userName', userController.getUserByName);
-
-router.delete('/:userName', userController.deleteUser);
+router.delete('/', userController.deleteUser);
 
 module.exports = router;
